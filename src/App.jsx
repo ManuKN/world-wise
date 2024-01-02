1345c62ac06ab9135844d3272532567dcca19a9f
Manu K N <201910101325@presidencyuniversity.in>
2024-01-02 17:22:39 +0530

    project is Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import Product from "./pages/Product"
-import Pricing from "./pages/pricing"
-import Homepage from "./pages/Homepage"
-import PageNotFound from "./pages/PageNotFound"
-import AppLayout from "./pages/AppLayout"
-import Login from "./pages/Login"
 import CityList from "./components/CityList"
 import CountryList from "./components/CountryList"
 import City from "./components/City"
 import Form from "./components/Form"
 import {CitiesProvider} from "./Contexts/CitiesProvider"
 import { AuthProvider } from "./Contexts/FakeAuthContext"
+import { Suspense, lazy } from "react"
+import SpinnerFullPage from "./components/SpinnerFullPage"
+// import Product from "./pages/Product"
+// import Pricing from "./pages/pricing"
+// import Homepage from "./pages/Homepage"
+// import PageNotFound from "./pages/PageNotFound"
+// import AppLayout from "./pages/AppLayout"
+// import Login from "./pages/Login"
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"))
+const Pricing = lazy(() => import("./pages/pricing"))
+const Login = lazy(()=> import("./pages/Login"))
+const AppLayout = lazy(()=>import("./pages/AppLayout"))
+const PageNotFound = lazy(()=> import("./pages/PageNotFound"))
 
 
 function App() {
@@ -19,6 +27,7 @@ function App() {
     <CitiesProvider>
       <AuthProvider>
    <BrowserRouter>
+   <Suspense fallback={<SpinnerFullPage />}>
    <Routes>
     <Route index element={<Homepage />}/>
     <Route path="Product" element={<Product />}/>
@@ -33,6 +42,7 @@ function App() {
     </Route>
     <Route path="*" element={<PageNotFound />}/>
    </Routes>
+   </Suspense>
    </BrowserRouter>
    </AuthProvider>
    </CitiesProvider>)
